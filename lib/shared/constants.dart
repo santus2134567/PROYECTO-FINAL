e839525a80ca6332da2f5be50c9809b878a0a26e
ProgramacionMovil app01 <201913176e@utea.edu.pe>
2022-12-22 18:22:28 -0500

    shared, contantes

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:986510847739:web:3ba4c4269f80c640364a5e";
+  static String apiKey = "AIzaSyBmc7x0H1KmyaJXMotND6k3ESEe9_eSu1w";
+  static String messagingSenderId = "986510847739";
+  static String projectId = "chatsappfirebase";
+  final primaryColor = Color.fromARGB(167, 28, 45, 195);
+}
